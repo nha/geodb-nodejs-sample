a9a70fd8f1c322d0778ec5c25dba1e4e48d43c17
nha <nha@users.noreply.github.com>
2018-07-15 22:37:10 +0100

    feat: simple test can require geodb

--- index.test.js
+++ index.test.js
@@ -1,15 +1,9 @@
-const geodb = require('geodb').api;
+const geodb = require('geodb');
 const websocket = require('websocket');
 
 const USER_TOKEN=process.env.GEODB_USER_TOKEN;
 const API_KEY=process.env.GEODB_API_KEY;
 
-
-
-function sum(a, b) {
-  return a + b;
-}
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('geodb is awesome', () => {
+  expect(geodb.api.awesome).toBe(true);
 });
